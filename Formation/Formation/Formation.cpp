de9235bbf21973b50a853c0e62bcea40c8986b67
BruceLane <bruce@batchass.fr>
2020-06-29 11:18:31 +0200

    ptr

--- Formation/Formation/Formation.cpp
+++ Formation/Formation/Formation.cpp
@@ -2,26 +2,31 @@
 //
 
 #include <iostream>
-using namespace std;
+//using namespace std;
 
 using Dimension = int; // peut être changer en double
 Dimension Longueur;
 Dimension Largeur;
 
 enum class Couleur: int8_t { ROUGE = 1, VERT, ORANGE };
+void f() {};
+void f(int) {};
+void f(float) {};
 int main()
 {
 	Couleur c{ Couleur::VERT };
-	string s{ "chaine" };
+	std::string s{ "chaine" };
 	Largeur = 3;
-	long long ll{ 67LL };
-	int n{};
+	long long ll{ 67'000'000LL };
+	int n{ 0b001 }, i{ 4 };
 	bool b{ 1 };
-	int p = n;
+	int* p = {&n};
+	
+	std::cin >> i;
 	if (b) {
-		std::cout << b << "vrai" << endl;
+		std::cout << b << "vrai" << p << i << std::endl;
 	}
 	else {
-		std::cout << b << "faux" << endl;
+		std::cout << b << "faux" << std::endl;
 	}
 }
