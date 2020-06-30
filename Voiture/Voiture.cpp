2a0977af0b9f539bf891b299ba3fc589e95d98bb
BruceLane <bruce@batchass.fr>
2020-06-30 16:13:50 +0200

    avant Moteur

--- Voiture/Voiture.cpp
+++ Voiture/Voiture.cpp
@@ -6,6 +6,7 @@ using namespace std;
 using Vitesse = int;
 using Acceleration = Vitesse;
 using Deceleration = Vitesse;
+using Immatriculation = string;
 // Ctrl+D Dupliquer la selection
 enum class Couleur { ROUGE, VERT, POURPRE };
 enum class Cylindree {CC_200, CC_250, CC_500, CC_2000, CC_4200};
@@ -14,6 +15,7 @@ private:
 	Couleur couleur;
 	const Cylindree cylindree;
 	const Vitesse VITESSE_MAXIMUM = 129;
+	const Immatriculation immatriculation;
 	Vitesse vitesse{ 0 };
 	void SetVitesse(Vitesse vitesse) {
 		if (vitesse < VITESSE_MAXIMUM) {
@@ -23,9 +25,10 @@ private:
 	}
 
 public:
-	Voiture(Couleur couleur, Cylindree cylindree) :
+	Voiture(Couleur couleur, Cylindree cylindree, Immatriculation immatriculation) :
 		couleur { couleur },
-		cylindree{ cylindree }
+		cylindree{ cylindree },
+		immatriculation{ immatriculation }
 	{
 		
 	}
@@ -46,12 +49,14 @@ public:
 
 int main()
 {
-	Voiture v1{ Couleur::ROUGE, Cylindree::CC_2000 };
+	Voiture v1{ Couleur::ROUGE, Cylindree::CC_2000, "BA-134" };
 	v1.demarrer(Vitesse{ 30 });
 	v1.accelerer(Acceleration{ 15 });
 	v1.ralentir(Deceleration{ 30 });
 	v1.stopper();
 
-
+	string s; // creation objet
+	s = "t"; // puis init
+	string s2{"r"}; // mieux pour performance
 }
 
